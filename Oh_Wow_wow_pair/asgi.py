a27c460f13a8b1e9c10df5d3df8c063cf889f132
EnLiao <pikachu200594@gmail.com>
2025-04-16 20:49:39 +0800

    init

---
+++ Oh_Wow_wow_pair/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Oh_Wow_wow_pair project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Oh_Wow_wow_pair.settings')
+
+application = get_asgi_application()
