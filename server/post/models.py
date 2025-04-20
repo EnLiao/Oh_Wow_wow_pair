758563967443da8c38fd8b94a79ad1c099f3b099
EnLiao <pikachu200594@gmail.com>
2025-04-21 03:01:56 +0800

    create Posts

--- server/post/models.py
+++ server/post/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+import uuid
 from core.models import User, Doll, Tag, DollTag
 
-# Create your models here.
+class Posts(models.Model):
+    post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    doll = models.ForeignKey(Doll, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    image_url = models.URLField(blank=True)
+    visibility = models.CharField(max_length=10, choices=[('public', 'Public'), ('private', 'Private')], default='public')
+    created_at = models.DateTimeField(auto_now_add=True)
 
+"""
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    post_id = 
+"""
