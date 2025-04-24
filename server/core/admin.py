ee3b5ba294d42ec2009eb418b35ffaf9ad556d58
noyapoyo <scot0228@gmail.com>
2025-04-24 11:39:58 +0800

    feat: Show doll, dollTag, tag in admin interface

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Doll, DollTag, Tag
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Doll)
+admin.site.register(Tag)
+admin.site.register(DollTag)
