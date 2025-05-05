25eee50300baaa393b7f93bbd18e600ec8b4d6e0
happylittle7 <lance20501@gmail.com>
2025-05-05 22:15:43 +0800

    Adds display customization to Doll admin
    Improves the Doll admin interface by adding a custom
    `DollAdmin` class. This allows specifying which fields
    are displayed in the list view, enhancing usability.

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import User, Doll, DollTag, Tag
 
+
+class DollAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'birthday')
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Doll)
+admin.site.register(Doll, DollAdmin)
 admin.site.register(Tag)
 admin.site.register(DollTag)
