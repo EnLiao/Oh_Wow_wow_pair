8fdc3282b4863c7b35c8df72715414a615019ab5
EnLiao <pikachu200594@gmail.com>
2025-05-06 14:18:33 +0800

    add post list in PostListView and add follow in core/admin.py

--- server/post/views.py
+++ server/post/views.py
@@ -26,7 +26,22 @@ class PostListView(ListAPIView):
         doll_id = self.request.query_params.get('doll_id')
         if not doll_id:
             return Post.objects.none()
-        followed_doll_ids = Follow.objects.filter(from_doll_id=doll_id).values_list('to_doll_id', flat=True)
+        #followed_doll_ids = Follow.objects.filter(from_doll_id=doll_id).values_list('to_doll_id', flat=True)
         seen_post_ids = PostSeen.objects.filter(doll_id=doll_id).values_list('post_id', flat=True)
 
-        return Post.objects.filter(doll_id__in=followed_doll_ids).exclude(id__in=seen_post_ids).order_by('-created_at')
+        #return Post.objects.filter(doll_id__in=followed_doll_ids).exclude(id__in=seen_post_ids).order_by('-created_at')
+        return Post.objects.exclude(id__in=seen_post_ids).order_by('-created_at')
+    
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        
+        doll_id = self.request.query_params.get('doll_id')
+        post_ids = [post['id'] for post in response.data]
+
+        seen_objects = [
+            PostSeen(doll_id=doll_id, post_id=post_id)
+            for post_id in post_ids
+        ]
+        PostSeen.objects.bulk_create(seen_objects, ignore_conflicts=True)  # 避免重複加入
+
+        return response
