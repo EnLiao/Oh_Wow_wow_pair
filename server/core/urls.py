367bd57e6aef16d11dda49a60e90583668a19128
noyapoyo <scot0227@gmail.com>
2025-05-06 20:21:30 +0800

    I update API of doll detail and follower and fans

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import DollListCreateView, TagListCreateView, DollDetailView
+from .views import DollListCreateView, TagListCreateView, DollDetailView, FollowersListView, FollowersToListView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('dolls/', DollListCreateView.as_view(), name='doll-list-create'),
     path('dolls/<str:id>/', DollDetailView.as_view(), name='doll-detail'),
+    path('dolls/<str:id>/followers/', FollowersListView.as_view(), name='doll-followers'),
+    path('dolls/<str:id>/follower_to/', FollowersToListView.as_view(), name='doll-follower_to'),
     path('tags/', TagListCreateView.as_view(), name='tag-list-create'),
 ]
