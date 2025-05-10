1d829c57fc5e00ef18808f1dacbeeceda76cb03b
noyapoyo <scot0227@gmail.com>
2025-05-10 16:51:17 +0800

    Add uploading image setting

--- server/core/urls.py
+++ server/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import DollListCreateView, TagListView, DollDetailView, FollowersListView, FollowersToListView, UserDollListView, FollowView
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
@@ -14,3 +16,4 @@ urlpatterns = [
     path('users/<str:username>/dolls/', UserDollListView.as_view(), name='user-dolls'),
     path('tags/', TagListView.as_view(), name='tag-list'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
