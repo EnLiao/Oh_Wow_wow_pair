fd338a18273ee142c4797ba6aeb7aeee36ee1c3b
noyapoyo <scot0227@gmail.com>
2025-05-18 00:59:56 +0800

    I fix get img bug.

--- server/core/urls.py
+++ server/core/urls.py
@@ -4,7 +4,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import DollListCreateView, TagListView, DollDetailView, FollowersListView, FollowersToListView, UserDollListView, FollowView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib import admin
+
 urlpatterns = [
+    #path('admin/', admin.site.urls),
+    #path('core/', include('core.urls')),
+    #path('post/', include('post.urls')),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
