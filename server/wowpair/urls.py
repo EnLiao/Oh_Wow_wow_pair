90deb08d618bdc4a42ddb3d5d9078a281ba4067d
noyapoyo <scot0227@gmail.com>
2025-05-22 18:50:08 +0800

    Follow Copilot suggesion.

--- server/wowpair/urls.py
+++ server/wowpair/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('post/', include('post.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
