e174bebc4fb2f3a7b723566ab620e0e80fdfd237
cyucccx <yuyuchen2005@gmail.com>
2025-06-06 23:50:47 +0800

    fix create post bugs

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -20,7 +20,7 @@ export const login = (data) => api.post('/core/login/', data);
 export const register = (data) => api.post('/core/register/', data);
 export const doll_list_view = (username) => api.get(`/core/users/${username}/dolls/`)
 export const create_doll = (data) => api.post('/core/dolls/', data);
-export const create_post = (data) => api.post('/post/posts', data);
+export const create_post = (data) => api.post('/post/posts/', data);
 export const getDollInfo = (dollId) => api.get(`/core/dolls/${dollId}/`);
 export const get_tags = () => api.get('/core/tags/');
 export async function getPosts({
