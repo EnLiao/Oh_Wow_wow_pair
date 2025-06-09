693604f384eb2118752f2ea058476e99d06cec70
EnLiao <pikachu200594@gmail.com>
2025-06-09 10:48:12 +0800

    feat: add search dollid feature

--- server/search/urls.py
+++ server/search/urls.py
@@ -1,7 +1,8 @@
 # search/urls.py
 from django.urls import path
 from . import views
+from .views import DollIdSearchAPIView
 
 urlpatterns = [
-    
+    path('doll-ids/', DollIdSearchAPIView.as_view(), name='search-doll-ids'),
 ]
--- server/search/views.py
+++ server/search/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from core.models import Doll
 
-# Create your views here.
+class DollIdSearchAPIView(APIView):
+    def get(self, request):
+        keyword = request.GET.get('q', '').strip()
+        if not keyword:
+            return Response({'results': []})
+
+        dolls = Doll.objects.filter(id__icontains=keyword).order_by('id')[:10]
+        ids = [d.id for d in dolls]
+        return Response({'results': ids})
