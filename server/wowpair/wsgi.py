98a7da6711af46081838a9efd00f5e5c5139282d
happylittle7 <lance20501@gmail.com>
2025-06-13 17:10:21 +0800

    Correct the Django settings module name in the ASGI and WSGI configurations.

--- server/wowpair/wsgi.py
+++ server/wowpair/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Oh_Wow_wow_pair.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wowpair.settings')
 
 application = get_wsgi_application()
