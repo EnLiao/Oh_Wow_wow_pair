939bfef8f265c56d61ec221beed0c8709d56c6f9
noyapoyo <scot0227@gmail.com>
2025-06-15 12:01:59 +0800

    Refactor serializers.py to include additional imports for email handling and caching

--- server/wowpair/settings.py
+++ server/wowpair/settings.py
@@ -15,8 +15,6 @@ from datetime import timedelta
 import logging
 import environ
 import os
-
-import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
