1b6511113f88a49fb0477d53fad210e6ffc69d2b
noyapoyo <scot0227@gmail.com>
2025-06-15 23:46:29 +0800

    Handle verify error's error message.

--- server/core/views.py
+++ server/core/views.py
@@ -31,6 +31,14 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class DollListCreateView(generics.ListCreateAPIView):
     queryset = Doll.objects.all()
     serializer_class = DollSerializer
