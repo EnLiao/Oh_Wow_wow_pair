6b16c18a4cd0afcf53dfe2d580fb144d00f06e6e
noyapoyo <scot0227@gmail.com>
2025-06-23 01:58:16 +0800

    clear non-verify daphne users.

--- server/core/apps.py
+++ server/core/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
+import sys
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # 啟動伺服器後自動定期清理未驗證用戶
+        if 'runserver' in sys.argv or 'daphne' in sys.argv:
+            import threading
+            def delete_unverified_users_periodically():
+                import time
+                from django.core.management import call_command
+                while True:
+                    try:
+                        call_command('delete_unverified_users')
+                    except Exception as e:
+                        pass  # 可加 log
+                    time.sleep(60)
+            t = threading.Thread(target=delete_unverified_users_periodically, daemon=True)
+            t.start()
