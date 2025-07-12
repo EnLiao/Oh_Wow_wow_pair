68c4ae50e23806792d02a57033f3eb786a46002b
EnLiao <pikachu200594@gmail.com>
2025-07-12 20:42:26 +0800

    feat: add notification to admin page

--- server/notifications/admin.py
+++ server/notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Notification
 
 # Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('doll', 'type', 'actor', 'is_read', 'created_at')
+    list_filter = ('type', 'is_read', 'created_at')
+    search_fields = ('doll__name', 'actor__name', 'content')
+    ordering = ('-created_at',)
