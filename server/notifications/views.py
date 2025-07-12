943b50d72cd39d37d5caf8667572afc392d94ccc
EnLiao <pikachu200594@gmail.com>
2025-07-12 22:28:24 +0800

    feat: add views.py

--- server/notifications/views.py
+++ server/notifications/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
 
 # Create your views here.
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all().order_by('-created_at')
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated] 
